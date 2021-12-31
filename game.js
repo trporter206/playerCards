26d8338570b4942c6096858346f64b1a480be901
trporter206 <tr.porter206@gmail.com>
2021-12-31 14:05:48 -0800

    added simple game object and refactored

--- game.js
+++ game.js
@@ -29,5 +29,35 @@ function Player(firstName, lastName, gender, age, city, primarySport) {
   }
 }
 
-const p1 = new Player('Torri', 'Porter', 'male', 26, 'vancouver', 'basketball')
-console.log(p1.fullName())
+function Game(type, team1, team2) {
+  this.type = type;
+  this.team1 = team1;
+  this.team2 = team2;
+  this.score = [0,0]
+
+  this.getGameType = function() { return this.type };
+  this.setGameType = function(value) { this.type = value };
+  this.getTeams = function() { return [this.team1, this.team2] };
+  this.getScore = function() { return this.score }
+
+  this.setTeams = function(value1, value2) {
+                    this.team1 = value1;
+                    this.team2 = value2;
+  }
+
+  this.addPlayer = function(player) {
+    this.playerList.push(player);
+    return this.playerList;
+  }
+
+  this.removePlayer = function(player) {
+    this.playerList.splice(this.playerList.indexOf(player), 1)
+    return this.playerList;
+  }
+}
+
+const p1 = new Player('Torri', 'Porter', 'male', 26, 'vancouver', 'basketball');
+const p2 = new Player('Nikki', 'Layson', 'male', 29, 'vancouver', 'running');
+
+const g1 = new Game('basketball', [p1], [p2])
+console.log(g1.getTeams())

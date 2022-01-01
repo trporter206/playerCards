be580044a89ad1cd6b38b3f4a5ef209ef85a1e07
trporter206 <tr.porter206@gmail.com>
2021-12-31 20:11:05 -0800

    added basic team functionality

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ function Player(firstName, lastName, gender, age, city, primarySport) {
   this.losses = 0;
   this.ties = 0;
   this.wlRatio = 0;
+  this.teams = []
 
   this.getFirstName = function() { return this.firstName };
   this.setFirstName = function(value) { this.firstName = value };
@@ -27,6 +28,10 @@ function Player(firstName, lastName, gender, age, city, primarySport) {
 
   this.getAge = function() { return this.age };
   this.setAge = function(value) { this.age = value };
+
+  this.getTeam = function() { return this.team };
+  this.setTeams = function(value) { this.team = value };
+
 
   this.getwlRatio = function() { return this.wlRatio };
 
@@ -56,6 +61,39 @@ function Player(firstName, lastName, gender, age, city, primarySport) {
   }
 }
 
+function Team(name, players, city) {
+  this.name = name;
+  this.players = players;
+  this.city = city;
+  this.wins = 0;
+  this.losses = 0;
+  this.ties = 0;
+  this.wlRatio = 0;
+
+  this.getPlayers = function() { return this.players }
+  this.getCity = function() { return this.city }
+  this.setCity = function(newCity) { this.city = newCity }
+
+  this.getRecord = function() {
+    return `${this.wins} - ${this.losses} - ${this.ties}`
+  }
+
+  this.updateRatio = function() {
+    const ratio = this.wins / this.losses
+    this.wlRatio = ratio
+  }
+
+  this.addPlayer = function(player) {
+    this.players.push(player)
+  }
+
+  this.removePlayer = function(player) {
+    this.players.splice(this.players.indexOf(player), 1)
+  }
+
+
+}
+
 function Game(type, team1, team2) {
   this.type = type;
   this.team1 = team1;
@@ -73,16 +111,6 @@ function Game(type, team1, team2) {
                     this.team2 = value2;
   }
 
-  this.addPlayer = function(player) {
-    this.playerList.push(player);
-    return this.playerList;
-  }
-
-  this.removePlayer = function(player) {
-    this.playerList.splice(this.playerList.indexOf(player), 1)
-    return this.playerList;
-  }
-
   this.generateScore = function() {
     const score1 = Math.floor(Math.random() * 10)
     const score2 = Math.floor(Math.random() * 10)
@@ -91,39 +119,34 @@ function Game(type, team1, team2) {
 
   this.determineWinners = function() {
     if (this.score[0] === this.score[1]){
-      for(i=0;i<this.team1.length;i++){
-        this.team1[i].addTie()
-        this.team2[i].addTie()
+      for(i=0;i<this.team1.players.length;i++){
+
+        this.team1.players[i].addTie()
+        this.team2.players[i].addTie()
       }
       this.winner = 'Tie'
       return `It's a tie game with a score of ${this.score[0]} - ${this.score[1]}`
     } else if (this.score[0] > this.score[1]) {
-      for(i=0;i<this.team1.length;i++){
-        this.team1[i].addWin()
-        this.team1[i].updateRatio()
-        this.team2[i].addLoss()
-        this.team2[i].updateRatio()
+      for(i=0;i<this.team1.players.length;i++){
+        this.team1.players[i].addWin()
+        this.team1.players[i].updateRatio()
+        this.team2.players[i].addLoss()
+        this.team2.players[i].updateRatio()
       }
       this.winner = team1
-      return `${this.score[0]} - ${this.score[1]}, ${this.team1[0].firstName} wins!`
+      return `${this.score[0]} - ${this.score[1]}, ${this.team1.name} wins!`
     } else {
-      for(i=0;i<this.team1.length;i++){
-        this.team1[i].addLoss()
-        this.team1[i].updateRatio()
-        this.team2[i].addWin()
-        this.team2[i].updateRatio()
+      for(i=0;i<this.team1.players.length;i++){
+        this.team1.players[i].addLoss()
+        this.team1.players[i].updateRatio()
+        this.team2.players[i].addWin()
+        this.team2.players[i].updateRatio()
       }
       this.winner = team2
-      return `${this.score[0]} - ${this.score[1]}, ${this.team2[0].firstName} wins!`
+      return `${this.score[0]} - ${this.score[1]}, ${this.team2.name} wins!`
     }
   }
 }
-
-
-
-
-
-
 
 
 const p1 = new Player('Torri', 'Porter', 'male', 26, 'vancouver', 'basketball');
@@ -131,3 +154,11 @@ const p2 = new Player('Nikki', 'Layson', 'male', 29, 'vancouver', 'running');
 const p3 = new Player('Alison', 'Parker', 'female', 24, 'singapore', 'baseball');
 const p4 = new Player('Indigo', 'Ward', 'female', 22, 'LA', 'rollerblading');
 const p5 = new Player('Manny', 'Sign', 'male', 25, 'India', 'cricket');
+const p6 = new Player('Rachel', 'Lui', 'female', 26, 'Phillipines', 'soccer');
+
+const team1 = new Team('Hawks', [p1,p2,p3], 'Seattle')
+const team2 = new Team('Bears', [p4,p5,p6], 'Chicago')
+
+const g1 = new Game('basketball', team1, team2)
+g1.generateScore()
+console.log(g1.determineWinners())

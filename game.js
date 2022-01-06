187c881f1cd58b458dd781d0daea81acd5089aaa
trporter206 <tr.porter206@gmail.com>
2022-01-06 13:14:29 -0800

    addfriend function improved, court form added to page

--- game.js
+++ game.js
@@ -7,26 +7,26 @@ const playerFactory = (firstName, lastName, gender, age, favoriteSport, maxLevel
     age: age,
     favoriteSport: favoriteSport,
     maxLevel: maxLevel,
-    friends: [],
+    friends: {},
 
     //get and set functions
-    getFirstName() { return firstName },
-    setFirstName(value) { firstName = value },
-    getLastName() { return lastName },
-    setLastName(value) { lastName = value },
-    getGender() { return gender },
-    setGender(value) { gender = value },
-    getCity() { return city },
-    setCity(value) { city = value },
-    getFavoriteSport() { return favoriteSport },
-    setFavoriteSport(value) { favoriteSport = value },
-    getAge() { return age },
-    setAge(value) { age = value },
-    getFriends() { return friends },
+    getFirstName() { return this.firstName },
+    setFirstName(value) { this.firstName = value },
+    getLastName() { return this.lastName },
+    setLastName(value) { this.lastName = value },
+    getGender() { return this.gender },
+    setGender(value) { this.gender = value },
+    getCity() { return this.city },
+    setCity(value) { this.city = value },
+    getFavoriteSport() { return this.favoriteSport },
+    setFavoriteSport(value) { this.favoriteSport = value },
+    getAge() { return this.age },
+    setAge(value) { this.age = value },
+    getFriends() { return this.friends },
 
     //return full name
     fullName() {
-      return `${firstName} ${lastName}`
+      return `${this.firstName} ${this.lastName}`
     },
 
     addCourt(name, address, type, hours) {
@@ -44,14 +44,17 @@ const playerFactory = (firstName, lastName, gender, age, favoriteSport, maxLevel
       return `Could not find ${courtName}`
     },
 
-    addFriend(playerFirstName) {
-      for (i=0; i<playerList.length; i++) {
-        if (playerList[i].firstName === playerFirstName) {
-          this.friends.push(playerList[i])
-          return `${playerList[i].firstName} has been added to your friends`;
+    addFriend(playerName) {
+      const players = Object.keys(playerList)
+      for (i=0; i<players.length; i++) {
+        const currentPlayer = playerList[players[i]]
+        console.log(currentPlayer.fullName())
+        if (currentPlayer.fullName() === playerName) {
+          this.friends[`${currentPlayer.fullName()}`] = currentPlayer
+          return `${currentPlayer.fullName()} has been added to your friends`;
         }
       }
-      return `Could not find ${playerFirstName}`
+      return `Could not find ${playerName}`
     }
   }
 }
@@ -64,21 +67,21 @@ const courtFactory = (name, address, activePlayers, type, hours) => {
     type: type,
     hours: hours,
 
-    getName() { return name },
-    setName(newName) { name = newName },
-    getAddress() { return address },
-    setAddress(newAddress) { address = newAddress },
-    getActivePlayers() { return activePlayers },
-    getType() { return type },
-    setType(newType) { type = newType },
-    getHours() { return hours },
-    setHours(newHours) { hours = newHours },
+    getName() { return this.name },
+    setName(newName) { this.name = newName },
+    getAddress() { return this.address },
+    setAddress(newAddress) { this.address = newAddress },
+    getActivePlayers() { return this.activePlayers },
+    getType() { return this.type },
+    setType(newType) { this.type = newType },
+    getHours() { return this.hours },
+    setHours(newHours) { this.hours = newHours },
 
   }
 }
 
-playerList = []
-courtList = []
+playerList = {}
+courtList = {}
 
 function getPlayerFormInfo(){
   var form = document.getElementById('player-form');
@@ -91,8 +94,23 @@ function getPlayerFormInfo(){
     var favoriteSport = this.elements.sport.value
     var maxLevel = this.elements.maxlevel.value
     let newPlayer = playerFactory(firstName, lastName, gender, age, favoriteSport, maxLevel)
-    playerList.push(newPlayer)
+    playerList[`${newPlayer.fullName()}`] = newPlayer
   })
 }
 
 getPlayerFormInfo()
+
+function getCourtFormInfo(){
+  var form = document.getElementById('court-form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    var courtName = this.elements.courtname.value
+    var address = this.elements.address.value
+    var type = this.elements.type.value
+    var hours = this.elements.hours.value
+    let newCourt = courtFactory(courtName, address, type, hours)
+    courtList[`${newCourt.name}`] = newCourt
+  })
+}
+
+getCourtFormInfo()
